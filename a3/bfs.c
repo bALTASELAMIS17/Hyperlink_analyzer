04de5fe233d9ab83b181cad80e9fcc13f8d3df34
raymondlin336 <94279301+raymondlin336@users.noreply.github.com>
2026-03-30 11:27:14 -0400

    working bfs

---
+++ a3/bfs.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include "parser.h"
+
+#define MAX_LINKS 1000
+#define MAX_URL_LEN 100
+
+void initialize_processes_file(){
+    int fd = open("processes.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    int zero = 0;
+    write(fd, &zero, sizeof(int));
+    close(fd);
+}
+
+bool process_limit_check(int max_processes){
+    char *filepath = "processes.txt";
+    int fd = open(filepath, O_RDWR);
+    if (fd == -1){
+        perror("open");
+        exit(1);
+    }
+    int processes = -1;
+    if (read(fd, &processes, sizeof(int)) != sizeof(int)){
+        perror("read");
+        close(fd);
+        exit(1);
+    }
+    printf("Currently running processes: %d\n", processes);
+    if (processes >= max_processes) {
+        close(fd);
+        return false;
+    }
+    else{
+        processes++;
+        if (lseek(fd, 0, SEEK_SET) == -1) {
+            perror("lseek");
+            close(fd);
+            exit(1);
+        }
+
+        if (write(fd, &processes, sizeof(int)) != sizeof(int)) {
+            perror("write");
+            close(fd);
+            exit(1);
+        }
+
+        close(fd);
+        return true;
+    }
+}
+
+void process_end(){
+    char *filepath = "processes.txt";
+    int fd = open(filepath, O_RDWR);
+    if (fd == -1){
+        perror("open");
+        exit(1);
+    }
+    int processes = -1;
+    if (read(fd, &processes, sizeof(int)) != sizeof(int)){
+        perror("read");
+        close(fd);
+        exit(1);
+    }
+    if (processes > 0) {
+        processes--;
+        if (lseek(fd, 0, SEEK_SET) == -1){
+            perror("lseek");
+            close(fd);
+            exit(1);
+        }
+        if (write(fd, &processes, sizeof(int)) != sizeof(int)){
+            perror("write");
+            close(fd);
+            exit(1);
+        }
+    }
+    close(fd);
+}
+
+void free_hyperlinks(char **labels, char **links, int num_links){
+    for (int i = 0; i < num_links; i++) {
+        free(labels[i]);
+        free(links[i]);
+    }
+    free(labels);
+    free(links);
+}
+
+int breadth_first_search(char *current_url, char *end_url, int depth) {
+    char **urls = NULL;
+    char **labels = NULL;
+
+    int num_links = hyperlink_analyzer(current_url, &labels, &urls);
+    printf("%d neighbors found for %s\n", num_links, current_url);
+
+    if (num_links <= 0) {
+        free_hyperlinks(labels, urls, num_links);
+        return -1;
+    }
+
+    // Maximum depth limit
+    if (depth >= 2) {
+        printf("Depth of 3 reached, quit\n");
+        free_hyperlinks(labels, urls, num_links);
+        return -1;
+    }
+
+    // Check all neighbors
+    for (int i = 0; i < num_links; i++) {
+        printf("%s\n", urls[i]);
+
+        // See if there is a match
+        if (strcmp(end_url, urls[i]) == 0) {
+            printf("======================== MATCH IS FOUND\n");
+            free_hyperlinks(labels, urls, num_links);
+            return depth + 1;
+        }
+    }
+
+    // Creates pipes
+    int pipes[num_links][2];
+    int children_pids[num_links];
+    int num_children = 0;
+
+    // Loop through all neighbors
+    for (int i = 0; i < num_links; i++) {
+        // TODO: check whether urls[i] has already been visited in the external file
+        // If already visited, continue
+        // If not, write it into the file
+
+        // TODO: check current number of running processes
+
+        // Make pipe
+        if (pipe(pipes[num_children]) == -1) {
+            perror("pipe");
+            free_hyperlinks(labels, urls, num_links);
+            return -1;
+        }
+
+        // Fork
+        int r = fork();
+        if (r < 0) {
+            perror("fork");
+            close(pipes[num_children][0]);
+            close(pipes[num_children][1]);
+            free_hyperlinks(labels, urls, num_links);
+            return -1;
+        }
+
+        // Child: close read end, conduct search, write depth result to pipe
+        if (r == 0) {
+            close(pipes[num_children][0]);
+
+            printf("Child searching %s at depth %d\n", urls[i], depth + 1);
+            int result = breadth_first_search(urls[i], end_url, depth + 1);
+            if (write(pipes[num_children][1], &result, sizeof(int)) != sizeof(int)) {
+                perror("write");
+                close(pipes[num_children][1]);
+                exit(1);
+            }
+
+            close(pipes[num_children][1]);
+            exit(0);
+        }
+
+        // Parent: store childs pid for use later, close write end
+        children_pids[num_children] = r;
+        close(pipes[num_children][1]);
+        num_children++;
+    }
+
+    // Given the pid of the child processes, wait for all of them to finish.
+    // If any of them returns non -1 and its currently -1, or if they have a better result, change best result
+    int best_result = -1;
+    for (int i = 0; i < num_children; i++) {
+        int child_result = -1;
+        if (read(pipes[i][0], &child_result, sizeof(int)) != sizeof(int)) {
+            perror("read");
+            child_result = -1;
+        }
+        close(pipes[i][0]);
+        waitpid(children_pids[i], NULL, 0);
+
+        if (child_result != -1) {
+            if (best_result == -1 || child_result < best_result) {
+                best_result = child_result;
+            }
+        }
+    }
+
+    free_hyperlinks(labels, urls, num_links);
+    return best_result;
+}
+
+int main(void){
+    int result = breadth_first_search("https://en.wikipedia.org/wiki/Webber_Academy", "https://wikipedia.org/wiki/Western_Canada", 0);
+    printf("Final result: %d\n", result);
+    return 0;
+}
--- a3/parser.h
+++ a3/parser.h
@@ -1,10 +1,10 @@
-#ifndef PARSER_H 
+#ifndef PARSER_H
 #define PARSER_H
 
 #include <stdio.h>
 
 // Checks if label is unique
-int label_exists(char **labels, int count, const char *label); 
+int label_exists(char **labels, int count, const char *label);
 
 // Stores unique hyperlink labels and their links and returns how many were found
 int extract_hyperlinks(const char *filepath, char ***labels_out, char ***links_out);
@@ -12,7 +12,7 @@ int extract_hyperlinks(const char *filepath, char ***labels_out, char ***links_o
 // Strips html tags to find label names
 void strip_html_tags(char *text);
 
-// Frees memory for hyperlink labels and links 
+// Frees memory for hyperlink labels and links
 void free_all(char **labels, char **links, int count);
 
 // Takes in a wikipedia link and returns hyperlink count and stores links and labels
